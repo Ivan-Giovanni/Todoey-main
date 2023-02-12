4c5b49c63f49c9c5496c4a0ab52bde19ab73c073
Ivan-Giovanni <zanguegiovanni@gmail.com>
2023-02-12 15:20:44 +0100

    CategoryViewController, set TableView data source methods complete

--- Todoey/CategoryViewController.swift
+++ Todoey/CategoryViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 import CoreData
 
 class CategoryViewController: UITableViewController {
+    
+    let categoryArray = ["Home", "Work", "Personal","Food"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +20,22 @@ class CategoryViewController: UITableViewController {
     }
     
     //MARK: -TableView DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return categoryArray.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categoryArray[indexPath.row]
+        
+        return cell
+        
+    }
     
     
     //MARK: - Data MAnipulation Methods
